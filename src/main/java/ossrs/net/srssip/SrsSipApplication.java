52ffcf8f5f2ccb522770d972d0788f6525952233
stormbirds <xbaojun@gmail.com>
2022-03-21 01:08:31 +0800

    Modify the api documentation

--- src/main/java/ossrs/net/srssip/SrsSipApplication.java
+++ src/main/java/ossrs/net/srssip/SrsSipApplication.java
@@ -1,13 +1,20 @@
 package ossrs.net.srssip;
 
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import ossrs.net.srssip.config.SRSStreamCasterConfig;
 import ossrs.net.srssip.config.SipConfig;
+import ossrs.net.srssip.config.SmsConfig;
 
 @SpringBootApplication
-@EnableConfigurationProperties({SipConfig.class, SRSStreamCasterConfig.class})
+@EnableConfigurationProperties({SipConfig.class, SmsConfig.class, SRSStreamCasterConfig.class})
 public class SrsSipApplication {
 
     public static void main(String[] args) {
--- src/main/java/ossrs/net/srssip/config/SwaggerConfig.java
+++ src/main/java/ossrs/net/srssip/config/SwaggerConfig.java
@@ -1,9 +1,9 @@
 package ossrs.net.srssip.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,18 +17,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public OpenAPI customOpenAPI(@Value("1.0") String appVersion) {
-        return new OpenAPI()
-                .info(new Info()
+    public GroupedOpenApi customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+        String[] paths = { "/api/v1/**" };
+        return GroupedOpenApi.builder().
+                group("v1")
+                .addOpenApiCustomiser(openApi -> openApi.info(new Info()
                         .title("SRS-SIP API v1.0")
                         .description("SRS Sip Server")
                         .version(appVersion)
+                        .termsOfService("https://github.com/ossrs/srs-sip")
                         .license(new License()
                                 .name("MIT")
                                 .url("https://github.com/ossrs/srs-sip/blob/main/LICENSE"))
                         .contact(new Contact()
                                 .name("stormbirds")
                                 .email("xbaojun@gmail.com")
-                                .url("https://github.com/ossrs/srs-sip")));
+                                .url("https://github.com/ossrs/srs-sip"))))
+                .pathsToMatch(paths)
+                .build();
     }
 }
